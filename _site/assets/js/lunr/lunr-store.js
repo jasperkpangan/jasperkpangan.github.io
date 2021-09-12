59a29f832447f32e18eb7db27114589d6f8054eb
unknown <jasper.pangan@yahoo.com>
2021-09-12 12:12:11 +0800

    Add site

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Pinoy Meets World: Exploratory Data Analysis on Filipino Emigration",
+        "excerpt":"This is our final project for our Data Visualization and Storytelling class. This was created alongside Jelo Delariarte, Sasha Garcia, Leo Lorenzo, and Hezi Rance. We were tasked to present a data visualization using any dataset of our choice. Executive Summary Filipinos have established a global community in an increasingly...","categories": ["projects"],
+        "tags": ["Filipino emigration","exploratory data analysis","tableau","python"],
+        "url": "/projects/filipino-emigration",
+        "teaser": null
+      },{
+        "title": "Cancelledt Culture:",
+        "excerpt":" ","categories": ["projects"],
+        "tags": ["PH Customs","exploratory data analysis","python"],
+        "url": "/projects/ph-grab-ml",
+        "teaser": null
+      }]
